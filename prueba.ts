49e29d489f2321a946ff360db574bc76ee8c4a61
Issac <74998947+issacbrizuela1@users.noreply.github.com>
2024-11-19 23:05:19 -0600

    mas ejemplos de com usar el decorador

--- prueba.ts
+++ prueba.ts
@@ -1,8 +1,6 @@
 
 
-/**
- * para solo hacer interfaces decoradas en lugar de programar el listado de opciones
- */
+
 const functionsMap: { [key: string]: string[] } = {};
 
 function listFunctions(target: any) {
@@ -15,9 +13,39 @@ function listFunctions(target: any) {
 
 @listFunctions
 class MiInterfaz {
-    funcion1() {}
-    funcion2() {}
-    funcion3() {}
+    funcion1() { }
+    funcion2() { }
+    funcion3() { }
 }
 
 console.log(functionsMap);
+
+function log(target: any, propertyKey: string, descriptor?: PropertyDescriptor): PropertyDescriptor | void {
+    if (descriptor === undefined) {
+      descriptor = Object.getOwnPropertyDescriptor(target, propertyKey);
+    }
+    console.log(descriptor)
+    console.log(target)
+    console.log(propertyKey)
+    if (descriptor && typeof descriptor.value === 'function') {
+      const originalMethod = descriptor.value;
+      descriptor.value = function (...args: any[]) {
+        console.log(`Llamada a ${propertyKey} con argumentos: ${JSON.stringify(args)}`);
+        const result = originalMethod.apply(this, args);
+        console.log(`Resultado de ${propertyKey}: ${result}`);
+        return result;
+      };
+      return descriptor;
+    }
+  }
+  
+  class Ejemplo {
+    @log
+    suma(a: number, b: number) {
+      return a + b;
+    }
+  }
+  
+  const ejemplo = new Ejemplo();
+  ejemplo.suma(2, 3);
+  
